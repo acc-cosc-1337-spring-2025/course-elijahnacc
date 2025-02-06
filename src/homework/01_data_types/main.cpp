70ccbba2c5c9d719f266b259382e86a29201486a
elijahnacc <elijah.newlon@g.austincc.edu>
2025-02-06 00:48:41 +0000

    hw1 : multiply numbers

--- src/homework/01_data_types/main.cpp
+++ src/homework/01_data_types/main.cpp
@@ -1,12 +1,27 @@
 ﻿//write include statements
+#include "data_types.h"
 
 
 //write namespace using statement for cout
+using std::cout;
+using std::cin;
 
 
 int main()
 {
+	int num;
+	cout<<"Enter number: ";
+	cin>>num; //grabs value from keyboard input
+
+	int result = multiply_numbers(num);
+
+	cout<<"Result: "<<result<<"\n";
+
+	int num1 = 4;
+
+	result = multiply_numbers(num1);
 	
+	cout<<"Result "<<result<<"\n";
 
 	return 0;
 }
