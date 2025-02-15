23c22bb9ca9ae257da4ba699e65c1d0d9d62192b
elijahnacc <elijah.newlon@g.austincc.edu>
2025-02-15 00:32:30 +0000

    hw2: restaurant bill

--- src/homework/02_expressions/hwexpressions.cpp
+++ src/homework/02_expressions/hwexpressions.cpp
@@ -7,7 +7,16 @@ int add_numbers(int num1, int num2)
 }
 
 //write function code here
+double get_sales_tax_amount(double meal_amount)
+{
+	const double tax_rate = 0.0675;
 
+	return meal_amount * tax_rate;
+}
 
-
-
+double get_tip_amount(double meal_amount, double tip_rate)
+{
+	tip_rate = tip_rate / 100;
+	
+	return meal_amount * tip_rate;
+}
--- src/homework/02_expressions/main.cpp
+++ src/homework/02_expressions/main.cpp
@@ -1,11 +1,36 @@
 ﻿//write include statements
+#include <iostream>
+#include <string>
+#include "hwexpressions.h"
 
 
 //write namespace using statement for cout
+using std::cin;
+using std::cout;
+using std::endl;
 
 int main()
 {
+	double meal_amount, tip_rate, tip_amount, tax_amount, total;
+
+	cout << "Enter meal amount: ";
+	cin >> meal_amount;
+
+	tax_amount = get_sales_tax_amount(meal_amount);
 	
+	cout << "Enter tip rate: ";
+	cin >> tip_rate;
+
+	tip_amount = get_tip_amount(meal_amount, tip_rate);
+
+	total = meal_amount + tip_amount + tax_amount;
+
+	cout << endl;
+	cout << "Meal Amount: " << meal_amount << endl;
+	cout << "Sales Tax: " << tax_amount << endl;
+	cout << "Tip Amount: " << tip_amount << endl;
+	cout << "- - - - - - - - - - - -" << endl;
+	cout << "Total: " << total << endl;
 
 	return 0;
 }
