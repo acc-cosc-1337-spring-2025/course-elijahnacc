f91e81557a059dee32d081fd871d4b3b5b2cb542
elijahnacc <elijah.newlon@g.austincc.edu>
2025-02-22 07:26:42 +0000

    hw3: letter grade if/switch

--- src/homework/03_decisions/decisions.cpp
+++ src/homework/03_decisions/decisions.cpp
@@ -1,4 +1,58 @@
 //write include statement for decisions header
+#include <string>
+#include <decisions.h>
+using std::string;
 
 
 //Write code for function(s) code here
+string get_letter_grade_using_if(int grade)
+{
+    string letter_grade;
+
+    if (grade < 0 or grade > 100) {
+        letter_grade = "Out of range";
+    }
+    else if (grade >= 90) {
+        letter_grade = "A";
+    }
+    else if (grade >= 80) {
+        letter_grade = "B";
+    }
+    else if (grade >= 70) {
+        letter_grade = "C";
+    }
+    else if (grade >= 60) {
+        letter_grade = "D";
+    }
+    else {
+        letter_grade = "F";
+    }
+    
+    return letter_grade;
+}
+
+string get_letter_grade_using_switch(int grade)
+{
+    string letter_grade;
+
+    switch (grade)
+    {
+        case 90 ... 100:
+            letter_grade = "A";
+            break;
+        case 80 ... 89:
+            letter_grade = "B";
+            break;
+        case 70 ... 79:
+            letter_grade = "C";
+            break;
+        case 60 ... 69:
+            letter_grade = "D";
+            break;
+        case 0 ... 59:
+            letter_grade = "F";
+            break;
+        default: letter_grade = "Out of range";
+    }
+    return letter_grade;
+}
--- src/homework/03_decisions/main.cpp
+++ src/homework/03_decisions/main.cpp
@@ -1,6 +1,49 @@
 //write include statements
+#include <iostream>
+#include <string>
+#include "decisions.h"
+
+using std::cin;
+using std::cout;
+using std::endl;
 
 int main() 
 {
+	int choice, grade;
+	string letter_grade;
+
+	cout << "Menu" << endl;
+	cout << "1 - Letter grade using if" << endl;
+	cout << "2 - Letter grade using switch" << endl;
+	cout << "3 - Exit" << endl;
+	
+	cout << "Enter menu selection: ";
+	cin >> choice;
+
+	switch (choice) {
+		case 3:
+			exit(0);
+		case 2:
+			cout << "Enter grade value (0-100): ";
+			cin >> grade;
+			letter_grade = get_letter_grade_using_switch(grade);
+			break;
+		case 1:
+			cout << "Enter grade value (0-100): ";
+			cin >> grade;
+			letter_grade = get_letter_grade_using_if(grade);
+			break;
+		default:
+			cout << "Invalid selection, closing program...";
+			exit(0);
+	}
+
+	if (letter_grade == "Out of range") {
+		cout << "Grade value out of range" << endl;
+	}
+	else {
+		cout << "The letter grade for value " << grade << " is " << letter_grade << endl;
+	}
+
 	return 0;
 }
